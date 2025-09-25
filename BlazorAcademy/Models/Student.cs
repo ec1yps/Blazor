97c9b69baf34f9ecd562a535ca41a1187ec2cb8e
ec1yps_ <ec1yps@yandex.ru>
2025-09-25 17:37:29 +0300

    Students page added

---
+++ BlazorAcademy/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
+
+namespace BlazorAcademy.Models
+{
+	public class Student
+	{
+		[Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int stud_id { get; set; }
+		
+		[Required, StringLength(50)]
+		public string? last_name { get; set; }
+		
+		[Required, StringLength(50)]
+		public string? first_name { get; set; }
+		
+		[StringLength(50)]
+		public string? middle_name { get; set; }
+		
+		[Required, DataType(DataType.Date)]
+		public DateTime? birth_date { get; set; }
+		
+		[StringLength(50)]
+		public string? email { get; set; }
+
+		[StringLength(16)]
+		public string? phone { get; set; }
+
+		public byte[]? photo { get; set; }
+
+		public int group { get; set; }
+	}
+}
